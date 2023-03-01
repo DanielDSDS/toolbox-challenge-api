f6fa8fef6b1c2d489436e781202317550f275d95
Daniel Di Mella <bigbigbyned@gmail.com>
2023-03-01 11:32:56 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const http = require('http');
+const morgan = require('morgan');
+const cors = require('compression');
+const compression = require('cors');
+
+// Startup
+const app = express();
+dotenv.config();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+if (app.get("env") === "production") {
+  app.use(compression());
+  app.use(morgan("common"));
+} else {
+  app.use(morgan("dev"));
+}
+const server = http.createServer(app);
+
+server.listen(port, (err) => {
+  if (err) throw err;
+  console.log('Server started at ' + port);
+});
