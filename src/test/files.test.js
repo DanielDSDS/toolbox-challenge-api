a08614269e0de46c09ce5958510272f21267d0ec
Daniel Di Mella <bigbigbyned@gmail.com>
2023-03-04 00:14:30 -0400

    Modify tests according to endpoint development

--- src/test/files.test.js
+++ src/test/files.test.js
@@ -7,7 +7,8 @@ chai.should();
 chai.use(chaihttp);
 
 describe("CSV files API", () => {
-  // Tests /secret/files endpoint 
+
+  // Tests /files/data endpoint 
   describe("GET secret file list", () => {
     // Debe retornar un arreglo de archivos 
     it("should return an array of files", (done) => {
@@ -21,38 +22,24 @@ describe("CSV files API", () => {
         })
     })
 
-    it("should fail when not specifying a bearer token", (done) => {
+    // Debe retornar un arreglo de archivos 
+    it("should return a specific file", (done) => {
       chai.request(server)
         .get('/files/data')
-        .end((err, res) => {
-          expect(res).to.have.status(401);
-          done();
-        })
-    })
-  })
-
-  // Tests /secret/file/{name} endpoint 
-  describe("GET secret file", () => {
-    // Debe funcionar al especificar un nombre de archivo 
-    it("should succeed when passing a specific file through query params", (done) => {
-      chai.request(server)
-        .get('/files/data')
+        .query({ fileName: 'test18.csv' })
         .set({ 'Authorization': 'Bearer aSuperSecretKey' })
-        .query({ fileName: 'test1.csv' })
         .end((err, res) => {
           expect(res).to.have.status(200);
-          expect(res.body).to.be.a('string');
-          expect(res.body).to.be.empty;
+          expect(res.body).to.be.a('object');
           done();
         })
     })
 
-    // Debe fallar al no encontrar el archivo especificado 
-    it("should fail when not specifying a file name", (done) => {
+    // Debe retornar un error de no conseguido 
+    it("should return a not found error", (done) => {
       chai.request(server)
-        .get('/files/data')
+        .get('/files/data?fileName=randomName')
         .set({ 'Authorization': 'Bearer aSuperSecretKey' })
-        .query({ fileName: 'non-existent test' })
         .end((err, res) => {
           expect(res).to.have.status(404);
           done();
@@ -60,4 +47,27 @@ describe("CSV files API", () => {
     })
   })
 
+  // Tests /files/list endpoint 
+  describe("GET secret file", () => {
+
+    // Debe retornar la informacion igual que la api externa 
+    it("should return same values as external API", (done) => {
+      chai.request('https://echo-serv.tbxnet.com/v1')
+        .get('/secret/files')
+        .set({ 'Authorization': 'Bearer aSuperSecretKey' })
+        .end((err, res) => {
+          expect(res.body).to.be.a('object');
+          chai.request(server)
+            .get('/files/list')
+            .set({ 'Authorization': 'Bearer aSuperSecretKey' })
+            .query({ fileName: 'test1.csv' })
+            .end((err, resp) => {
+              expect(resp).to.have.status(200);
+              expect(resp.body).to.be.a('object');
+              expect(JSON.stringify(resp.body) === JSON.stringify(res.body)).to.be.true;
+              done();
+            })
+        })
+    })
+  })
 })
